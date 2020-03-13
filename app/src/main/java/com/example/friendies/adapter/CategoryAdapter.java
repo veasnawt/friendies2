bc343004f2d01b93c2556e434b1e25a9a1988887
suyngorn <suyngorn9999@gmail.com>
2020-03-13 22:36:43 +0700

    Updated Cagegory

--- app/src/main/java/com/example/friendies/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/friendies/adapter/CategoryAdapter.java
@@ -1,4 +1,47 @@
 package com.example.friendies.adapter;
 
-public class CategoryAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.friendies.R;
+import com.example.friendies.holder.CategoryHolder;
+import com.example.friendies.model.CategoryModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryHolder> {
+
+    Context context;
+    ArrayList<CategoryModel> categoryList;
+
+    public CategoryAdapter(Context context, ArrayList<CategoryModel> categoryList) {
+        this.context = context;
+        this.categoryList = categoryList;
+    }
+
+    @NonNull
+    @Override
+    public CategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.more_item_category,null);
+
+        return new CategoryHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryHolder holder, int position) {
+        holder.textViewTitle.setText(categoryList.get(position).getTitle());
+        Picasso.get().load(categoryList.get(position).getUrl_img()).into(holder.url_img);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
 }
