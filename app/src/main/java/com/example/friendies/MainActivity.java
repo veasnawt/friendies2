309e2a66133ad3c0c8f79b6444af2f403240a657
suyngorn <suyngorn9999@gmail.com>
2020-03-25 22:39:33 +0700

    profile

--- app/src/main/java/com/example/friendies/MainActivity.java
+++ app/src/main/java/com/example/friendies/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.SearchView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
